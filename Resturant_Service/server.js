c029dc24cb50997ace4897acb750350ef9a6a55b
Kavin <kavinmadusanka20011@gmail.com>
2025-04-23 11:13:14 +0530

    create database connection

--- Resturant_Service/server.js
+++ Resturant_Service/server.js
@@ -1,6 +1,14 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
+//config env
+dotenv.config();
+
+//database congit
+connectDB();
 
 //rest object
 const app = express();
@@ -12,7 +20,7 @@ app.get("/", (req, res) => {
 })
 
 //port
-const PORT = 8086
+const PORT = process.env.PORT || 8086;
 
 app.listen(PORT, () => {
     console.log(`server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
