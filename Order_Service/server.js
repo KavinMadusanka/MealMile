b96e5d6d6aa7bc88f6ef8b3e1ce2ec599261efe4
Navishka Gunawardana <128272519+navishkaG@users.noreply.github.com>
2025-04-23 20:36:20 +0530

    Create orderRoutes and orderControllers

--- Order_Service/server.js
+++ Order_Service/server.js
@@ -3,6 +3,9 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 8087;
 
+app.use(express.json());
+app.use("/api/orders", require("./routes/orderRoutes"));
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
