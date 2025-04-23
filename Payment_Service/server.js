2740db88eb9d54901f0f33626d3347a7dca1ff3a
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2025-04-23 21:18:32 +0530

    Create server.js

---
+++ Payment_Service/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+//import connectDB from './config/db.js';
+
+//config env
+dotenv.config();
+
+//database congit
+//connectDB();
+
+//rest object
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send({
+        message: "welcome to Payment server"
+    })
+})
+
+//port
+const PORT = process.env.PORT || 8087;
+
+app.listen(PORT, () => {
+    console.log(`server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+});
