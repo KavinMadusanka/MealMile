c54075bd391bdc0e6c72f3182c6296064bf4f6db
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2025-04-24 17:26:58 +0530

    Update server.js

--- Payment_Service/server.js
+++ Payment_Service/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
+import paymentRoutes from './routes/paymentRoutes.js'
 //import connectDB from './config/db.js';
 
 //config env
@@ -13,6 +14,19 @@ dotenv.config();
 //rest object
 const app = express();
 
+// Stripe webhook needs RAW body for that endpoint only
+app.use((req, res, next) => {
+    if (req.originalUrl === '/api/payments/stripe/webhook') {
+      express.raw({ type: 'application/json' })(req, res, next);
+    } else {
+      express.json()(req, res, next);
+    }
+  });
+  
+  app.use(morgan('dev'));
+  app.use('/api/payments', paymentRoutes);
+
+  //Root test
 app.get("/", (req, res) => {
     res.send({
         message: "welcome to Payment server"
