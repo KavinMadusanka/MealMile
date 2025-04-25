ed29a80403d8f0f473ae6ddd8763fbdd7ca4846e
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2025-04-25 16:23:36 +0530

    Updated frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PaymentPage from "./pages/payment/PaymentPage";
+import PaymentSuccess from "./pages/payment/PaymentSuccess";
+import PaymentCancel from "./pages/payment/PaymentCancel";
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      Home
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/payment/success" element={<PaymentSuccess />} />
+        <Route path="/payment/cancel" element={<PaymentCancel />} />
+      </Routes>
+    </Router>
   );
 }
 
