71a5221acfd81e5f220626ac92d204c14297c5e5
Prabhasari <piyushaprabhasari@gmail.com>
2025-04-25 07:19:02 +0530

    Created delivery service setup

---
+++ Delivery_Service/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+
+//config env
+dotenv.config();
+
+//database congit
+connectDB();
+
+//rest object
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send({
+        message: "welcome to Delivery managment server"
+    })
+})
+
+//port
+const PORT = process.env.PORT || 8090;
+
+app.listen(PORT, () => {
+    console.log(`server running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white);
+});
