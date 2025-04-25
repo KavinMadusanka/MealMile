aa4151f57a2d4d4d21b53a630492ae9eadc85653
Navishka Gunawardana <128272519+navishkaG@users.noreply.github.com>
2025-04-25 15:30:10 +0530

    create errorHandler.js

--- Order_Service/server.js
+++ Order_Service/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const colors = require("colors");
+const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 8087;
 
 app.use(express.json());
 app.use("/api/orders", require("./routes/orderRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`.bgCyan.white);
