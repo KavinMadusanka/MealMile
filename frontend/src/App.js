63c250ba528920490feab833463695df6a020d06
Kavin <kavinmadusanka20011@gmail.com>
2025-04-28 08:40:12 +0530

    wrap with toast for get toast messages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,9 +8,15 @@ import HomePage from './pages/HomePage';
 import Review from './pages/Review';
 import DeliveryDashboard from './pages/delivery/DeliveryDashboard';
 import RestaurantRegistration  from './pages/restaurant/RestaurantRegistration';
+import DeliveryHistory from './pages/delivery/DeliveryHistory';
+import DeliveryRequests from './pages/delivery/DeliveryRequests';
+import TrackingUpdates from './pages/delivery/TrackingUpdates';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
+    <>
+      <Toaster position="top-right" reverseOrder={false} />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/payment" element={<PaymentPage />} />
@@ -20,6 +26,7 @@ function App() {
         <Route path="/deliveryDashboard" element={<DeliveryDashboard />} />
         <Route path="/RestaurantRegistration" element={<RestaurantRegistration />} />
       </Routes>
+    </>
   );
 }
 
