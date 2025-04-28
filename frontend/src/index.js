c5d751a1fc9877808e310176c80e02ba9a916066
Kavin <kavinmadusanka20011@gmail.com>
2025-04-28 08:41:07 +0530

    remove unnecessary part

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,12 +15,6 @@ root.render(
   </AuthProvider>
 );
 
-// ReactDOM.render(
-//   <BrowserRouter>
-//     <App />
-//   </BrowserRouter>,
-//   document.getElementById('root')
-// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
