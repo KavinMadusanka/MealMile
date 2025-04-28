eafe6c72c48828c903bd496b87263f2dd9892379
Sandamini Indrachapa <130564612+SandaminiI@users.noreply.github.com>
2025-04-28 22:56:26 +0530

    Added cors

--- Order_Service/server.js
+++ Order_Service/server.js
@@ -3,12 +3,16 @@ const colors = require("colors");
 const errorHandler = require("./middleware/errorHandler");
 const dotenv = require("dotenv").config();
 const connectDB = require("./config/db");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 8089;
 
 //database congit
 connectDB();
+
+// CORS Middleware
+app.use(cors());
 
 app.use(express.json());
 app.use("/api/cart",require("./routes/cartRoutes"));
